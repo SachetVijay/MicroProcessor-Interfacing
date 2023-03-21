e7e5f717abfd18df7f1f0f2969c3d5abd85e1cdc
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-03-22 00:57:01 +0530

    Lab 1 Added

---
+++ lab1.asm
@@ -0,0 +1,8 @@
+                             MVI A,44
+                             
+                             MVI B,35
+                             
+                             ADD B
+                             OUT 01
+                             
+                             HLT
